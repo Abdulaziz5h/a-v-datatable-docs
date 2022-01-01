4b7856d7e6a49b88a588a1827fc8a820e2231bf9
AbdUlaziz shahrour <48730605+Abdulaziz5h@users.noreply.github.com>
2022-01-01 21:01:53 +0200

    .

--- src/core/layout/components/mini-footer.vue
+++ src/core/layout/components/mini-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-2 d-flex justify-content-between align-items-center">
-    <small>Copyright © 2022 - Present. Powered by <a href="https://github.com/Abdulaziz5h">Ab_00_d</a> with <span class="text-danger" style="font-size: 16px">❤</span>.</small>
+    <small>Copyright © 2021 - Present. Powered by <a href="https://github.com/Abdulaziz5h">Ab_00_d</a> with <span class="text-danger" style="font-size: 16px">❤</span>.</small>
     <ul class="list-unstyled p-0 m-0">
       <li><small><a href="https://github.com/Abdulaziz5h/a-v-datatable/blob/main/LICENSE.MD">MIT| License</a></small></li>
     </ul>
